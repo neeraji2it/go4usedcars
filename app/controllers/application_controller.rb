c50dd4b6538d635c0ccedbb426b37964f6efadf1
rails <rails@rails>
2014-12-16 12:28:29 +0530

    admin layouts with bootstrap features and added dashboards submenus and content with normal bootstrap design, for admin mgmt, installed devise, created devise views and model admin, etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,33 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :get_layout
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation,:name) }
+  end
+
+  def  after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+     admin_dashboards_path
+    end
+  end
+
+   def  after_sign_out_path_for(resource_or_scope)
+    root_url
+   end
+
+   protected
+
+  def get_layout
+    if devise_controller?
+      false
+    elsif self.class.parent == Admin
+      "admin"
+    else
+      "application"
+    end
+  end
 end
