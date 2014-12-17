850208b840f6c69544998d5f183e5fb19f881fa4
rails <rails@rails>
2014-12-17 14:02:14 +0530

    added migration to add car pic, make ajax to load all the varients with specific carmodel

--- app/controllers/admin/vehicles_controller.rb
+++ app/controllers/admin/vehicles_controller.rb
@@ -3,10 +3,11 @@ class Admin::VehiclesController < ApplicationController
     @vehicle = Vehicle.new
     @manufacturers = Manufacturer.all
     @car_models = CarModel.where("manufacturer_id=?", params[:manufacturer_id])
+    @varients = Varient.where("car_model_id=?", params[:car_model_id])
   end
 
   def create
-    @vehicle = Vehicle.new(vechicle_params)
+    @vehicle = Vehicle.new(vehicle_params.merge(:varient_id => params[:varient][:varient_id]))
     if @vehicle.save
       redirect_to new_admin_vehicle_path
     else
@@ -16,6 +17,10 @@ class Admin::VehiclesController < ApplicationController
 
   def load_car_model
     @car_models = CarModel.where("manufacturer_id=?", params[:manufacturer_id])
+  end
+
+  def load_varients
+    @varients = Varient.where("car_model_id=?", params[:car_model_id])
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217060203) do
+ActiveRecord::Schema.define(version: 20141217082115) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -136,6 +136,10 @@ ActiveRecord::Schema.define(version: 20141217060203) do
     t.string   "where_is_car"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
