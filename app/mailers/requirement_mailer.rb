449168dc9e47ec07fbd68f90ec50a5f27faddd32
rails <rails@rails.com>
2014-12-18 19:55:43 +0530

    reconfigure mailer and completed for associates partner

---
+++ app/mailers/requirement_mailer.rb
@@ -0,0 +1,40 @@
+class RequirementMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def send_requirement(
+        min_budget, max_budget, car_year, car_year1, make, 
+        model,body_type1,body_type2,body_type3,
+       	body_type4,body_type5,
+       	body_type6,body_type7,body_type8,body_type9,
+        fuel_type1,fuel_type2,fuel_type3,fuel_type4,name,email,phone
+        )
+        @min_budget = min_budget
+        @max_budget = max_budget
+        @car_year = car_year
+        @car_year1 = car_year1
+        @make = make
+        @model = model
+        @body_type1 = body_type1
+        @body_type2 = body_type2
+        @body_type3 = body_type3
+        @body_type4 = body_type4
+        @body_type5 = body_type5
+        @body_type6 = body_type6
+        @body_type7 = body_type7
+        @body_type8 = body_type8
+        @body_type9 = body_type9
+        @fuel_type1=fuel_type1
+        @fuel_type2=fuel_type2
+        @fuel_type3=fuel_type3
+        @fuel_type4=fuel_type4
+        @name=name
+        @mail=mail
+        @phone=phone
+        mail(:to =>"smaranreddy123@gmail.com", :subject => "User Requirements")
+  end
+
+  def associate_partner(associate_partner)
+    @associate = AssociatePartner.find(associate_partner.id)
+    mail(:to =>"smaranreddy123@gmail.com", :subject => "User Requirements")
+  end
+end
--- test/mailers/requirement_mailer_test.rb
+++ test/mailers/requirement_mailer_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RequirementTest < ActionMailer::TestCase
+class RequirementMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
