b1b6a3be690379508c11d9623cd6e1208952ad59
rails <rails@rails>
2014-12-23 19:46:18 +0530

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,16 +70,16 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
- config.action_mailer.delivery_method = :smtp
- config.action_mailer.default_url_options = { :host => 'http://go4usedcars.herokuapp.com' }
-  ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'http://go4usedcars.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
     #    :domain => 'gmail.com',
     :port => '587',
     :user_name => 'dinemediahousing@gmail.com',
     :password => 'dinemediapassword',
-    :authentication => 'plain'
+    :authentication => 'plain',
+    :enable_starttls_auto => true
   }
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
