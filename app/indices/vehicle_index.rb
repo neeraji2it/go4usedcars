51d51ba09425b5aee0b903ecef3ef20bf740108a
rails <rails@rails>
2015-01-01 13:59:55 +0530

    small changes in index file

--- app/indices/vehicle_index.rb
+++ app/indices/vehicle_index.rb
@@ -4,9 +4,6 @@ ThinkingSphinx::Index.define(:vehicle, :with => :active_record) do
   indexes :manufactured_year
   indexes :milage
   indexes :exterior_color
-  # indexes :manufacturer_id
-  # indexes :varient_id
-  # indexes :car_model_id
   indexes :location, :sortable => true  
   indexes manufacturer(:name), :as => :manufacturer
   indexes car_model(:name), :as => :car_model
