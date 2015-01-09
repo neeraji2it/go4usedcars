035255ed0831cf12f1b8743034747b6a74f68fb4
darpandalal <darpandalal@gmail.com>
2015-01-09 16:46:20 +0530

    small changes

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -13,7 +13,7 @@ class Admin::DashboardsController < ApplicationController
   
   def approved
     @associate_partner = AssociatePartner.find(params[:id])
-    @user = User.where("email = #{@associate_partner.email}").first
+    @user = User.where("email = '#{@associate_partner.email}'").first
     @associate_partner.status == "Yes" ? @associate_partner.update_attributes(:status => "No") : @associate_partner.update_attributes(:status => "Yes")
     @user.approved == true ? (@user.update_attribute(:approved, false)) : (@user.update_attribute(:approved, true))
   end
