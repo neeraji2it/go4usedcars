b72c77670a09e6015ac6ebfd153f3d2c23add54b
rails <rails@rails>
2015-01-13 11:30:40 +0530

    omniauth keys for production added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '489309741123184', 'af02149e8eda81abc3249708f3aba81b', {:scope => 'email,publish_actions,offline_access'}
+  if Rails.env == 'development'
+    provider :facebook, '489309741123184', 'af02149e8eda81abc3249708f3aba81b', {:scope => 'email,publish_actions,offline_access'}
+  elsif Rails.env == 'production'
+    provider :facebook, '385660371605927', 'c4cbf53918c3bd9b3a692847138092e3', {:scope => 'email,publish_actions,offline_access'}
+  end
 end
