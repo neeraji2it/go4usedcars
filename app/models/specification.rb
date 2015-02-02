39358b23e97f5fd14d35871686142d63296d90c6
rails <rails@rails>
2015-02-02 16:21:32 +0530

    make specification unique && offer index order managed

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -2,5 +2,5 @@ class Specification < ActiveRecord::Base
   belongs_to :specification_category
   has_one :car_specification, :dependent => :destroy
   validates :specification_category_id, :presence => true 
-  validates :name, :presence => true 
+  validates :name, :presence => true , :uniqueness => true
 end
