fdf86ff1cb00d8d74cd8f15cbd343e01ca60199d
rails <rails@rails>
2015-02-02 15:29:01 +0530

    added dealer information in the dealer_cars

--- app/models/associate_partner.rb
+++ app/models/associate_partner.rb
@@ -1,3 +1,7 @@
 class AssociatePartner < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => true
+  
+  def self.user_partner(email)
+    where(:email => email).first
+  end
 end
