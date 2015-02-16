d0a1112a53980ef24b3ae6d1efd208fe3d6624b8
rails <rails@rails.com>
2015-02-16 12:18:58 +0530

    phone validation in sellcar

--- app/models/sell_car.rb
+++ app/models/sell_car.rb
@@ -2,11 +2,13 @@ class SellCar < ActiveRecord::Base
 	belongs_to :varient
   has_one :car_evaluation, :dependent => :destroy
 	has_attached_file :image, :styles => { :small => "500x500", :thumb => "100x100>" }, :default_url => "/assets/missing.png"
+
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :seller_phone, :presence => true, format: { with: /\A\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})\Z/,
     message: " Phone numbers must be in xxx-xxx-xxxx format" }
-  validates :image, :manufacture_year,:owner_type,:kms_done,:city,:color,:registration_no,:ownership,:car_insurence,:when_to_sell,:insurence_valid_till,:expected_price,:comments,:seller_name,:seller_email,:seller_organization, :presence => true
+  validates :image, :manufacture_year,:owner_type,:kms_done,:city,:color,:registration_no,:ownership,:car_insurence,:when_to_sell,:insurence_valid_till,:expected_price,:seller_name,:seller_email,:seller_organization, :presence => true
   validates :kms_done, :expected_price , numericality: {only_float: true}
+
   def model_varient
     "#{self.try(:varient).try(:name)} #{self.try(:varient).try(:model).try(:name)}"
   end
