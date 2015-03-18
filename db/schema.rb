1475171b4ef2d8f1166cb2cf73893b63e98f012e
rails <rails@rails>
2015-03-18 16:01:53 +0530

    config settings

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150213100726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -47,10 +50,10 @@ ActiveRecord::Schema.define(version: 20150213100726) do
     t.integer  "sell_car_id"
     t.string   "evaluator"
     t.text     "remarks"
-    t.float    "expected_price",                limit: 24
-    t.float    "evaluated_price",               limit: 24
-    t.float    "deal_price1",                   limit: 24
-    t.float    "deal_price2",                   limit: 24
+    t.float    "expected_price"
+    t.float    "evaluated_price"
+    t.float    "deal_price1"
+    t.float    "deal_price2"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "evaluation_image_file_name"
@@ -90,125 +93,6 @@ ActiveRecord::Schema.define(version: 20150213100726) do
     t.integer  "resume_file_size"
     t.datetime "resume_updated_at"
   end
-
-  create_table "comfy_cms_blocks", force: true do |t|
-    t.string   "identifier",                      null: false
-    t.text     "content",        limit: 16777215
-    t.integer  "blockable_id"
-    t.string   "blockable_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comfy_cms_blocks", ["blockable_id", "blockable_type"], name: "index_comfy_cms_blocks_on_blockable_id_and_blockable_type", using: :btree
-  add_index "comfy_cms_blocks", ["identifier"], name: "index_comfy_cms_blocks_on_identifier", using: :btree
-
-  create_table "comfy_cms_categories", force: true do |t|
-    t.integer "site_id",          null: false
-    t.string  "label",            null: false
-    t.string  "categorized_type", null: false
-  end
-
-  add_index "comfy_cms_categories", ["site_id", "categorized_type", "label"], name: "index_cms_categories_on_site_id_and_cat_type_and_label", unique: true, using: :btree
-
-  create_table "comfy_cms_categorizations", force: true do |t|
-    t.integer "category_id",      null: false
-    t.string  "categorized_type", null: false
-    t.integer "categorized_id",   null: false
-  end
-
-  add_index "comfy_cms_categorizations", ["category_id", "categorized_type", "categorized_id"], name: "index_cms_categorizations_on_cat_id_and_catd_type_and_catd_id", unique: true, using: :btree
-
-  create_table "comfy_cms_files", force: true do |t|
-    t.integer  "site_id",                                    null: false
-    t.integer  "block_id"
-    t.string   "label",                                      null: false
-    t.string   "file_file_name",                             null: false
-    t.string   "file_content_type",                          null: false
-    t.integer  "file_file_size",                             null: false
-    t.string   "description",       limit: 2048
-    t.integer  "position",                       default: 0, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comfy_cms_files", ["site_id", "block_id"], name: "index_comfy_cms_files_on_site_id_and_block_id", using: :btree
-  add_index "comfy_cms_files", ["site_id", "file_file_name"], name: "index_comfy_cms_files_on_site_id_and_file_file_name", using: :btree
-  add_index "comfy_cms_files", ["site_id", "label"], name: "index_comfy_cms_files_on_site_id_and_label", using: :btree
-  add_index "comfy_cms_files", ["site_id", "position"], name: "index_comfy_cms_files_on_site_id_and_position", using: :btree
-
-  create_table "comfy_cms_layouts", force: true do |t|
-    t.integer  "site_id",                                     null: false
-    t.integer  "parent_id"
-    t.string   "app_layout"
-    t.string   "label",                                       null: false
-    t.string   "identifier",                                  null: false
-    t.text     "content",    limit: 16777215
-    t.text     "css",        limit: 16777215
-    t.text     "js",         limit: 16777215
-    t.integer  "position",                    default: 0,     null: false
-    t.boolean  "is_shared",                   default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comfy_cms_layouts", ["parent_id", "position"], name: "index_comfy_cms_layouts_on_parent_id_and_position", using: :btree
-  add_index "comfy_cms_layouts", ["site_id", "identifier"], name: "index_comfy_cms_layouts_on_site_id_and_identifier", unique: true, using: :btree
-
-  create_table "comfy_cms_pages", force: true do |t|
-    t.integer  "site_id",                                         null: false
-    t.integer  "layout_id"
-    t.integer  "parent_id"
-    t.integer  "target_page_id"
-    t.string   "label",                                           null: false
-    t.string   "slug"
-    t.string   "full_path",                                       null: false
-    t.text     "content_cache",  limit: 16777215
-    t.integer  "position",                        default: 0,     null: false
-    t.integer  "children_count",                  default: 0,     null: false
-    t.boolean  "is_published",                    default: true,  null: false
-    t.boolean  "is_shared",                       default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comfy_cms_pages", ["parent_id", "position"], name: "index_comfy_cms_pages_on_parent_id_and_position", using: :btree
-  add_index "comfy_cms_pages", ["site_id", "full_path"], name: "index_comfy_cms_pages_on_site_id_and_full_path", using: :btree
-
-  create_table "comfy_cms_revisions", force: true do |t|
-    t.string   "record_type",                  null: false
-    t.integer  "record_id",                    null: false
-    t.text     "data",        limit: 16777215
-    t.datetime "created_at"
-  end
-
-  add_index "comfy_cms_revisions", ["record_type", "record_id", "created_at"], name: "index_cms_revisions_on_rtype_and_rid_and_created_at", using: :btree
-
-  create_table "comfy_cms_sites", force: true do |t|
-    t.string  "label",                       null: false
-    t.string  "identifier",                  null: false
-    t.string  "hostname",                    null: false
-    t.string  "path"
-    t.string  "locale",      default: "en",  null: false
-    t.boolean "is_mirrored", default: false, null: false
-  end
-
-  add_index "comfy_cms_sites", ["hostname"], name: "index_comfy_cms_sites_on_hostname", using: :btree
-  add_index "comfy_cms_sites", ["is_mirrored"], name: "index_comfy_cms_sites_on_is_mirrored", using: :btree
-
-  create_table "comfy_cms_snippets", force: true do |t|
-    t.integer  "site_id",                                     null: false
-    t.string   "label",                                       null: false
-    t.string   "identifier",                                  null: false
-    t.text     "content",    limit: 16777215
-    t.integer  "position",                    default: 0,     null: false
-    t.boolean  "is_shared",                   default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comfy_cms_snippets", ["site_id", "identifier"], name: "index_comfy_cms_snippets_on_site_id_and_identifier", unique: true, using: :btree
-  add_index "comfy_cms_snippets", ["site_id", "position"], name: "index_comfy_cms_snippets_on_site_id_and_position", using: :btree
 
   create_table "images", force: true do |t|
     t.integer  "vehicle_id"
@@ -251,7 +135,7 @@ ActiveRecord::Schema.define(version: 20150213100726) do
     t.datetime "image_updated_at"
     t.datetime "manufacture_year"
     t.string   "owner_type"
-    t.float    "kms_done",             limit: 24
+    t.float    "kms_done"
     t.string   "city"
     t.string   "color"
     t.string   "transmission"
@@ -260,7 +144,7 @@ ActiveRecord::Schema.define(version: 20150213100726) do
     t.string   "car_insurence"
     t.string   "when_to_sell"
     t.datetime "insurence_valid_till"
-    t.float    "expected_price",       limit: 24
+    t.float    "expected_price"
     t.text     "comments"
     t.string   "seller_name"
     t.string   "seller_email"
@@ -268,7 +152,7 @@ ActiveRecord::Schema.define(version: 20150213100726) do
     t.string   "seller_organization"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "status",                          default: "enquiry"
+    t.string   "status",               default: "enquiry"
   end
 
   create_table "sell_evaluations", force: true do |t|
@@ -364,13 +248,13 @@ ActiveRecord::Schema.define(version: 20150213100726) do
     t.string   "transmission"
     t.string   "registration_state"
     t.string   "registration_no"
-    t.float    "sell_price",         limit: 24
+    t.float    "sell_price"
     t.string   "sterio"
     t.string   "seat_covers"
     t.string   "car_for"
     t.string   "visible_status"
     t.string   "where_is_car"
-    t.float    "offer_price",        limit: 24
+    t.float    "offer_price"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_file_name"
