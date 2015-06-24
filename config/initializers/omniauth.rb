9ac7fb52902662f9c75670621191d6d2813e1cf6
Ranjana <rockingrails@gmail.com>
2015-06-24 18:49:07 +0530

    new cred created for omni

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env == 'development'
     provider :facebook, '489309741123184', 'af02149e8eda81abc3249708f3aba81b', {:scope => 'email,publish_actions,offline_access'}
   elsif Rails.env == 'production'
-    provider :facebook, '385660371605927', 'c4cbf53918c3bd9b3a692847138092e3', {:scope => 'email,publish_actions,offline_access'}
+    provider :facebook, '1617640621812845', '6bc7fbebf63619676538ee38d0baae29', {:scope => 'email,publish_actions,offline_access'}
   end
 end
