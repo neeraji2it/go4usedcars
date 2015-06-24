69e435dc8e6dfbecf927fc5345bae2c3406b2aba
Ranjana <rockingrails@gmail.com>
2015-06-24 19:05:53 +0530

    removed permission

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env == 'development'
-    provider :facebook, '489309741123184', 'af02149e8eda81abc3249708f3aba81b', {:scope => 'email,publish_actions,offline_access'}
+    provider :facebook, '489309741123184', 'af02149e8eda81abc3249708f3aba81b'
   elsif Rails.env == 'production'
-    provider :facebook, '1617640621812845', '6bc7fbebf63619676538ee38d0baae29', {:scope => 'email,publish_actions,offline_access'}
+    provider :facebook, '1617640621812845', '6bc7fbebf63619676538ee38d0baae29'
   end
 end
